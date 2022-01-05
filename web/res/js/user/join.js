e4dda62f6374661ae785c3768b2141836fe2fc93
jungwoogyun <jwg8910@naver.com>
2022-01-05 10:15:22 +0900

    아이디 중복 체크 작업 중

---
+++ web/res/js/user/join.js
@@ -0,0 +1,12 @@
+{
+    const joinFrmElem = document.querySelector('#join-frm');
+
+    if(joinFrmElem) {
+        //아이디 중복 체크 버튼
+        const idBtnChkElem = joinFrmElem.querySelector('#id-btn-chk');
+        idBtnChkElem.addEventListener('click', () => {
+            const idVal = joinFrmElem.uid.value;
+            alert(idVal);
+        });
+    }
+}
