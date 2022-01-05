1d64885d54501d8d395fad19370bae49c16fd9f2
jungwoogyun <jwg8910@naver.com>
2022-01-05 14:16:26 +0900

    아이디 중복 체크 프론트엔드 작업 중

--- web/res/js/user/join.js
+++ web/res/js/user/join.js
@@ -1,6 +1,18 @@
 {
     const joinFrmElem = document.querySelector('#join-frm');
 
+    const setIdChkMsg = (data) => {
+        const idChkMsgElem = joinFrmElem.querySelector('#id-chk-msg');
+        switch(data.result) {
+            case 0:
+                idChkMsgElem.innerText = '이미 사용중인 아이디 입니다.';
+                break;
+            case 1:
+                idChkMsgElem.innerText = '사용할 수 있는 아이디 입니다.';
+                break;
+        }
+    };
+
     if(joinFrmElem) {
         //아이디 중복 체크 버튼
         const idBtnChkElem = joinFrmElem.querySelector('#id-btn-chk');
@@ -14,7 +26,7 @@
             fetch(`/user/idChk/${idVal}`)
             .then(res => res.json())
             .then((data) => {
-                console.log(data);
+                setIdChkMsg(data);
             }).catch((e)=> {
                console.log(e);
             });
